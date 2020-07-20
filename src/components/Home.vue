6cfcdf7ba8e7db52fa46f1485617ca8bcf5f2811
Igor Tuag <igortuag@gmail.com>
2020-07-20 19:15:44 -0300

    Graphic interface

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container fluid>
+    <div class="pills">
+      <Pill v-for="word in groupedWords" :key="word.name" 
+        :name="word.name" :amount="word.amount" />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from './Pill';
+
+export default {
+  components: { Pill },
+  data: function () {
+      return {
+        groupedWords: [
+          { name: 'i', amount: 1234 },
+          { name: 'you', amount: 900 },
+          { name: 'he', amount: 853 },
+        ]
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Pill.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <span>{{ name }}</span>
+    <span>{{ amount  }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['name', 'amount']
+}
+</script>
+
+<style>
+
+</style>
