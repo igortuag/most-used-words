b27084c9b4e7c1132bc1d08061f4023ca1494b5c
Igor Tuag <igortuag@gmail.com>
2020-07-20 19:40:53 -0300

    Graphic interface input

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,33 +1,55 @@
 <template>
   <v-container fluid>
+    <v-form>
+      <v-file-input
+        label="Selecione as Legendas"
+        prepend-icon="mdi-message-text"
+        append-outer-icon="mdi-send"
+        outlined=""
+        multiple
+        chips
+        v-model="files"
+        @click:append-outer=processSubtitles
+      />
+    </v-form>
     <div class="pills">
-      <Pill v-for="word in groupedWords" :key="word.name" 
-        :name="word.name" :amount="word.amount" />
+      <Pill
+        v-for="word in groupedWords"
+        :key="word.name"
+        :name="word.name"
+        :amount="word.amount"
+      />
     </div>
   </v-container>
 </template>
 
 <script>
-import Pill from './Pill';
+import Pill from "./Pill";
 
 export default {
   components: { Pill },
-  data: function () {
-      return {
-        groupedWords: [
-          { name: 'i', amount: 1234 },
-          { name: 'you', amount: 900 },
-          { name: 'he', amount: 853 },
-        ]
-      }
-  }
-}
+  data: function() {
+    return {
+      files: [],
+      groupedWords: [
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+      ],
+    };
+  },
+  methods: {
+    processSubtitles() {
+      console.log(this.files);
+    },
+  },
+};
 </script>
 
 <style>
-  .pills {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-  }
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
 </style>
