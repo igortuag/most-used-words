fac5c1024c6776dd669493576678085021c985a4
Igor Tuag <igortuag@gmail.com>
2020-07-21 07:26:27 -0300

    Process pathsToRows

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,13 +1,17 @@
 const { ipcMain } = require("electron");
 
-ipcMain.on("process-subtitles", (event, paths) => {
-  console.log(paths);
+const pathsToRows = require("./pathsToRows");
 
-  event.reply("process-subtitles", [
-    { name: "i", amount: 1234 },
-    { name: "you", amount: 900 },
-    { name: "she", amount: 853 },
-    { name: "our", amount: 853 },
-    { name: "he", amount: 853 },
-  ]);
+ipcMain.on("process-subtitles", (event, paths) => {
+  pathsToRows(paths)
+    .then((rows) => console.log(rows))
+    .then(() => {
+      event.reply("process-subtitles", [
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "she", amount: 853 },
+        { name: "our", amount: 853 },
+        { name: "he", amount: 853 },
+      ]);
+    });
 });
