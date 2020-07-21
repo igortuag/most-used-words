5dee40ac41a2e0ccbd6f7d093a7ad285809eb673
Igor Tuag <igortuag@gmail.com>
2020-07-21 07:49:53 -0300

    groupWords

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,18 +2,11 @@ const { ipcMain } = require("electron");
 
 const pathsToRows = require("./pathsToRows");
 const prepareData = require("./prepareData");
+const groupWords = require("./groupWords");
 
 ipcMain.on("process-subtitles", (event, paths) => {
   pathsToRows(paths)
     .then((rows) => prepareData(rows))
-    .then(words => console.log(words))
-    .then(() => {
-      event.reply("process-subtitles", [
-        { name: "i", amount: 1234 },
-        { name: "you", amount: 900 },
-        { name: "she", amount: 853 },
-        { name: "our", amount: 853 },
-        { name: "he", amount: 853 },
-      ]);
-    });
+    .then((words) => groupWords(words))
+    .then((groupedWords) => event.reply("process-subtitles", groupedWords));
 });
